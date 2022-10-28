656a8524e977b4ea8880e7a7599d2964d73d4a77
Esteban <e.lopez7@hotmail.com>
2022-10-28 09:57:16 -0400

    Add project files.

---
+++ DigitalRaid/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DigitalRaid.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalRaid.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<DRUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
